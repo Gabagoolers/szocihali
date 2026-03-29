1531435b03118231b0d935424703ce40b7923628
Daniel Kovari <74988054+captnketchup@users.noreply.github.com>
2026-03-29 21:26:51 +0200

    Revert "test: fail import check"
    This reverts commit d24813a3d4a30f202132b0e37aeb2b28fd127b73.

--- src/main/java/hu/gabagoolers/szocihali/domain/user/controller/UserController.java
+++ src/main/java/hu/gabagoolers/szocihali/domain/user/controller/UserController.java
@@ -3,7 +3,11 @@ package hu.gabagoolers.szocihali.domain.user.controller;
 import hu.gabagoolers.szocihali.domain.user.entity.User;
 import hu.gabagoolers.szocihali.domain.user.service.UserService;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.*;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
 
